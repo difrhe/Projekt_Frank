a4e6793e16e32db998052516500fe37a1877f9eb
difrhe <FHessemer@T-Online.de>
2024-09-21 12:56:58 +0200

    Demo_11 new

---
+++ Demo_11.java
@@ -0,0 +1,26 @@
+/**
+ * Aktuelles Datum abfragen
+ *
+ * @author Dirk Louis
+ */
+import java.util.Date;
+import java.util.Calendar;
+import java.text.DateFormat;
+
+public class Demo_1
+{
+
+ public static void main(String args[])
+ {
+  System.out.println();
+
+  Date today = new Date();
+  System.out.println(today);
+
+  Calendar calendar = Calendar.getInstance();
+  System.console().printf("%s\n", DateFormat.getDateTimeInstance().format(calendar.getTime()) );
+  System.out.println();
+
+ }
+}
+
